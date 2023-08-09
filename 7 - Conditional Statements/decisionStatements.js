632ff19ba177f0d7ae50ce1c1cd5b1036e5c2024
arrrjhay009 <stamaria.ryanjay09@gmail.com>
2023-08-10 04:07:03 +0800

    Add decisionStatement.js

---
+++ 7 - Conditional Statements/decisionStatements.js
@@ -0,0 +1,55 @@
+// if
+// else.. if
+// else
+// switch
+// ternary
+
+
+// IF STATEMENT
+// var count = 4; // variable declaration and initialization
+// if (count == 4) { // condition statement if count is equal to 4 is true,
+//     console.log("Count is 4."); // execute this console
+// }
+
+// IF.. ELSE STATEMENT
+// var count = 3; 
+// if (count == 4) {
+//     console.log("Count is equals to 4.");
+// }
+// else {
+//     console.log("Count is NOT equals to 4.");
+// }
+
+// IF.. ELSE IF.. STATEMENT
+// var count = 3; 
+// if (count == 4) {
+//     console.log("Count is equals to 4.");
+// }
+// else if (count > 4) {
+//     console.log("Count is greater than 4.")
+// }
+// else if (count < 4) {
+//     console.log("Count is less than 4.")
+// }
+// else {
+//     console.log("Count is NOT equals to 4.");
+// }
+
+// SWITCH STATEMENT
+let hero = 'Superman';
+switch (hero) {
+    case 'Superman':
+        console.log('Super Strength');
+        console.log('X-Ray Vision');
+        break;
+    case 'Batman':
+        console.log('Intelligence');
+        console.log('Martial Arts');
+        break;
+    default:
+        console.log('Justice League');
+        break;
+}
+
+
+
